6bc8a6bd7ee073be96c8982bf03df1bed917d22e
JeremyBaumiller <jbaumil07@gmail.com>
2024-01-31 20:43:10 -0500

    upload vault

---
+++ script.js
@@ -0,0 +1,22 @@
+//The first step in unlocking the vault is to solve code1.
+
+//The second step in unlocking the vault is to solve code2.
+
+//The final step in unlocking the vault is to solve code3.
+
+alert(`"You have received this message because you have been chosen to open an important vault. Here is the secret combination:" `)
+
+function vaultCodes () {
+    let code1 = 5 + 5 
+    let code2 = 50 - 10
+    let code3 = 78 / 2
+
+//popup alert displays secret codes. 
+
+alert(`To unlock the vault, ${code1} ${code2} ${code3}`)
+
+ const greeting = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:" 
+
+}                                 
+vaultCodes ()
+
